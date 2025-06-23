5b69061372bc2d4babe79e823e9e71f9714f0460
Diasazrilparalon <diasazril223@gmail.com>
2025-06-23 12:16:39 +0700

    forum revisi

--- src/services/post/post_service.js
+++ src/services/post/post_service.js
@@ -12,44 +12,64 @@ module.exports = {
     });
   },
 
-  async getPostById(id) {
-    const foundPost = await Post.findByPk(id, {
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'name'],
-        },
-        {
-          model: comment,
-          include: [
-            {
-              model: reply_comment,
-              as: 'reply_comments',
-              include: [{ model: User, attributes: ['id', 'name' ] }]
-            },
-            {
-              model: User,
-              attributes: ['id', 'name'],
-            }
-          ]
-        },
-        {
-          model: reaction,
-          include: [{ model: User, attributes: ['id', 'name'] }]
-        }
-      ]
-    });
-
-    if (foundPost) {
-      foundPost.views += 1;
-      await foundPost.save();
-
-      // Urutkan komentar berdasarkan jumlah like terbanyak (jika ada kolom `like`)
-      if (foundPost.comments) {
-        foundPost.comments = foundPost.comments.sort((a, b) => (b.like || 0) - (a.like || 0));
+ async getPostById(id) {
+  const foundPost = await Post.findByPk(id, {
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'name'],
+      },
+      {
+        model: comment,
+        include: [
+          {
+            model: reply_comment,
+            as: 'reply_comments',
+            include: [{ model: User, attributes: ['id', 'name'] }]
+          },
+          {
+            model: User,
+            attributes: ['id', 'name'],
+          }
+        ]
+      },
+      {
+        model: reaction,
+        include: [{ model: User, attributes: ['id', 'name'] }]
       }
-    }
+    ]
+  });
 
-    return foundPost;
+  if (!foundPost) return null;
+
+  // Tambah views
+  foundPost.views += 1;
+  await foundPost.save();
+
+  // Urutkan komentar berdasarkan jumlah like (jika kolom like ada)
+  if (foundPost.comments) {
+    foundPost.comments = foundPost.comments.sort((a, b) => (b.like || 0) - (a.like || 0));
   }
+
+  // Hitung total dan per jenis reaksi
+  const reactions = foundPost.reactions || [];
+  const totalReactions = reactions.length;
+  const reactionCount = {};
+  for (const r of reactions) {
+    const type = r.type;
+    if (!reactionCount[type]) {
+      reactionCount[type] = 1;
+    } else {
+      reactionCount[type]++;
+    }
+  }
+
+  // Gabungkan hasil
+  return {
+    ...foundPost.toJSON(),
+    totalReactions,
+    reactionCount
+  };
+}
+
 };
